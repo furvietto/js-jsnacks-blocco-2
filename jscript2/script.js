34694e89fc4685260bab95bbb48eb9fa63a1dc5e
furvietto <fulvio-94@hotmail.it>
2021-11-30 12:22:12 +0100

    add function on jscript2

--- jscript2/script.js
+++ jscript2/script.js
@@ -1,12 +1,38 @@
 // jsnack 2
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 
-const numbers = [2,45,34,24,11,13,79,26]
-let result = 0;
-for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 == 1) {
-        result += numbers[i]
-    }
-}
+// lavoro
+// function dispariNumbers(array) {
+//    for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 == 1) {
+//         result += array[i]
+//     }        
+//    } 
+   
+// }
 
-console.log(result)
+// const numbers = [2,45,34,24,11,13,79,26]
+// let result = 0;
+
+// dispariNumbers(numbers)
+
+// console.log(result);
+
+// random integer
+// function getRndInteger(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1) ) + min;
+// }
+
+// let result = 0;
+// for (let i = 0; i < 10; i++) {
+//     let mate = getRndInteger(0,100);
+//     console.log(mate)
+//     if (mate % 2 == 1) {
+//         result += mate
+//     }
+// }
+
+// console.log(result)
+
+
+
