2f9e562dcb52af9447acbf1ce3668f5f3e917ecd
furvietto <fulvio-94@hotmail.it>
2021-11-30 11:52:01 +0100

    script1 add funciton

--- jscript1/script.js
+++ jscript1/script.js
@@ -1,6 +1,9 @@
 // jsnack 1
 // Generatore di “nomi cognomi” casuali: prendendo una lista di nomi e una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.
 
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
 let names = ["fulvio " , "daniele " , "piero " , "giorgio " , "marco " , "paolo "];
 
@@ -11,8 +14,8 @@ let empty = []
 
 
 while (empty.length < 6) {
-    let mathname = Math.floor(Math.random() * 5);
-    let mathsurname = Math.floor(Math.random() * 5);
+    let mathname = getRndInteger(0,5);
+    let mathsurname = getRndInteger(0,5);
     if (!empty.includes(names[mathname] + surname[mathsurname])) { 
         empty.push(names[mathname] + surname[mathsurname])
       }   
